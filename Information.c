b5e7aa1a0e79dca4bc27c34ac7c2e3236d0d547f
Raphi C. Escosora <raphiescosora80@gmail.com>
2026-09-04 11:50:11 +0400

    Escosora_Raphi_Performance Task

---
+++ Information.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("================================\n");
+    printf("       STUDENT INFORMATION        ");
+    printf("================================\n");
+    printf("Name: Raphi C. Escosora\n");
+    printf("Age: 17\n");
+    printf("Course: BSIT\n");
+    printf("Year Level: 1st Year\n");
+    printf("Favorite Programming Language: C\n");
+    printf("================================\n");
+    return 0;
+}
---
+++ profile.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name: Raphi C. Escosora\n");
+    printf("Age: 17\n");
+    printf("Course: BSIT\n");
+    printf("School: La Consolacion College Liloan, Cebu, Inc.\n");
+    return 0;
+}
+
+
